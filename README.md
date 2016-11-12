9f32250be41caac85aa25c7ad37a00942cc6e38d
Michal Ostruszka <michal.ostruszka@gmail.com>
2016-11-12 23:10:54 +0100

    Use Ansible vault to encrypt secrets (user/pass) from secrets.yml

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 #### Don't fear the devops - taming server infrastructure with Ansible
 
 This repo contains complete source code together with required vagrant infrastructure to run ansible provisioning against. Follow the commits in order.
+
+#### Ansible Vault
+
+This is just for demo purposes. **NEVER COMMIT YOUR VAULT PASSWORD FILES!!!**
+ 
+Vault password is `KE9hC8AbEyubFY`.
--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,18 @@
 let express = require('express');
+let basicAuth = require('express-basic-auth');
 let config = require(process.cwd() + '/config.json');
 
 let app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+
+app.use(basicAuth({
+  users: {
+    [config.ADMIN_USER]: config.ADMIN_PASSWORD
+  },
+  challenge: true
+}));
 
 app.get('/', (req, res) => res.render('index', {greeting: 'Hello BuildStuff LT', server: config.SERVER_ADDRESS}));
 app.listen(config.APP_PORT, () => console.log(`Server started on port ${config.APP_PORT}...`));
