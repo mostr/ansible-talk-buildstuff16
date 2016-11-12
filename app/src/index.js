f559af27f861d4f59299c07741607b175b318901
Michal Ostruszka <michal.ostruszka@gmail.com>
2016-11-12 22:56:58 +0100

    Introduce app config, use facts and group vars to populate config file template

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,10 @@
 let express = require('express');
+let config = require(process.cwd() + '/config.json');
 
 let app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-app.get('/', (req, res) => res.render('index', {greeting: 'Hello BuildStuff LT'}));
-app.listen(8080, () => console.log(`Server started on port 8080...`));
+app.get('/', (req, res) => res.render('index', {greeting: 'Hello BuildStuff LT', server: config.SERVER_ADDRESS}));
+app.listen(config.APP_PORT, () => console.log(`Server started on port ${config.APP_PORT}...`));
